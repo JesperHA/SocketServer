9799ed0dc292b6b7a49068106afb85cedc735df3
Jesper-PI <jelleholm91@gmail.com>
2020-01-19 22:20:51 +0100

    Created feature branch for makefiler_buffer

--- SocketServer.py
+++ SocketServer.py
@@ -7,13 +7,14 @@ HOST = ''  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
 xAxis = 0
-xAxisTemp = 0
 yAxis = 0
 zAxis = 0
 
 xServo = 17
 yServo = 27
 zServo = 22
+
+exceptionAmount = 1
 
 p = pigpio.pi()
 
@@ -25,61 +26,57 @@ p.set_PWM_frequency(xServo, 50)
 p.set_PWM_frequency(yServo, 50)
 p.set_PWM_frequency(zServo, 50)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    print("Listening...")
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        print("printing xAxis before input: ", xAxis)
-        # p.start(2.5)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        flag = True
+s.bind((HOST, PORT))
+s.listen()
+print("Listening...")
+conn, addr = s.accept()
+with conn:
+    print('Connected by', addr)
+    # p.start(2.5)
 
-        while flag:
-            data = conn.recv(1024)
-            print("Data recieved: ", data)
+    flag = True
 
-            string = str(data, 'utf8')
-            strings = string.split()
-            print(strings)
+    while flag:
+        data = conn.recv(1024)
 
-            try:
+        string = str(data, 'utf8')
+        strings = string.split()
 
-                if strings[0] == "xAxis":
-                    xAxis = float(strings[1])  # converting to float
-                if strings[2] == "yAxis":
-                    yAxis = float(strings[3])
-                if strings[4] == "zAxis":
-                    zAxis = float(strings[5])
-                if strings[0] == "q":
-                    p.set_PWM_dutycycle(xServo, 0)
-                    p.set_PWM_dutycycle(yServo, 0)
-                    p.set_PWM_dutycycle(zServo, 0)
-                    flag = False
-                if strings[0] == "f":
-                    p.set_PWM_dutycycle(xServo, 0)
-                    p.set_PWM_dutycycle(yServo, 0)
-                    p.set_PWM_dutycycle(zServo, 0)
-            except:
-                print("Failed input, continuing program")
+        try:
 
-            print("--------------")
-            print("****", xAxis, "****")
-            print(yAxis, "******", zAxis)
+            if strings[0] == "xAxis":
+                xAxis = float(strings[1])  # converting to float
+            if strings[2] == "yAxis":
+                yAxis = float(strings[3])
+            if strings[4] == "zAxis":
+                zAxis = float(strings[5])
+            if strings[0] == "q":
+                p.set_PWM_dutycycle(xServo, 0)
+                p.set_PWM_dutycycle(yServo, 0)
+                p.set_PWM_dutycycle(zServo, 0)
+                flag = False
 
-            try:
-                p.set_servo_pulsewidth(xServo, xAxis)
-                p.set_servo_pulsewidth(yServo, yAxis)
-                p.set_servo_pulsewidth(zServo, zAxis)
-                # time.sleep(0.25)
-            except KeyboardInterrupt:
-                p.stop()
+            if strings[0] == "f":
+                p.set_PWM_dutycycle(xServo, 0)
+                p.set_PWM_dutycycle(yServo, 0)
+                p.set_PWM_dutycycle(zServo, 0)
+        except:
+            print("Corrupt data, continuing program. Amount of corrupted data packets:  ", exceptionAmount)
+            exceptionAmount = exceptionAmount + 1
 
-            # conn.sendall(data)
+        try:
+            p.set_servo_pulsewidth(xServo, xAxis)
+            p.set_servo_pulsewidth(yServo, yAxis)
+            p.set_servo_pulsewidth(zServo, zAxis)
+            # time.sleep(0.25)
+        except KeyboardInterrupt:
+            p.stop()
 
-        s.close()
+        # conn.sendall(data)
+
+s.close()
 
 
 
