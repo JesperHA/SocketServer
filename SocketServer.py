81a64699a5b124b3cf5ae063301855d78f7eb47d
Jesper-PI <jelleholm91@gmail.com>
2020-01-22 14:15:25 +0100

    made video stream, very inefficient but no buffering

--- SocketServer.py
+++ SocketServer.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
+#!LD_PRELOAD=/usr/lib/arm-linux-gnueabihf/libatomic.so.1 python3
 
+import sys
 import socket
 import pigpio
 import time
+import cv2
+import base64
+import zlib
+import zmq
+import numpy as np
+
+sys.path.append('/usr/lib/python3/dist-packages')
+# remember to run pigpio daemon!
 
 HOST = ''  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
@@ -33,10 +43,13 @@ p.set_PWM_frequency(tServo, 50)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+
 s.bind((HOST, PORT))
 s.listen()
 print("Listening...")
 conn, addr = s.accept()
+
+video = cv2.VideoCapture("sample.mp4")
 
 with conn:
     print('Connected by', addr)
@@ -46,10 +59,32 @@ with conn:
     while flag:
         startTime = time.time()
         data = conn.recv(1024)
-        time.sleep(0.01)
+        # time.sleep(0.01)
 
         string = str(data, 'utf-8')
         strings = string.split()
+        try:
+            ret, frame = video.read()
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            frame = cv2.resize(frame, (427, 240))
+            # frame = cv2.resize(frame, (1280, 720))
+            sizeBefore = sys.getsizeof(frame)
+
+            encoded, buf = cv2.imencode(".jpg", frame)
+            image = base64.b64encode(buf)
+            sizeAfter = sys.getsizeof(image)
+            print("Size before: ", sizeBefore)
+            print("Size After: ", sizeAfter)
+
+            # image_string = str(image, 'utf-8')
+            # compressedImage = zlib.compress(image)
+            # size = sys.getsizeof(compressedImage)
+
+
+
+
+        except:
+            print("Video exception")
 
         try:
 
@@ -83,10 +118,18 @@ with conn:
         except KeyboardInterrupt:
             p.stop()
 
+        # print(image)
+
+        conn.sendall(image)
+        # print(value)
+        # conn.sendall(bytes("executed!", 'utf-8'))
         delay = time.time() - startTime
-        sleep = ((1 / 60) - delay % (1 / 60))
-        time.sleep(sleep)
-        conn.sendall(bytes("executed!", 'utf-8'))
+        # sleep = ((1 / 24) - delay % (1 / 24))
+        # time.sleep(sleep)
+        tickTime = delay
+        tickrate = 1 / tickTime
+        print("tickTime: ", tickTime)
+        print("tickrate: ", tickrate)
 
 s.close()
 
